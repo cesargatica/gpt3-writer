15ac2a351400e07010d0a35a55382a02e466d0b5
Cesar G. Gatica Rosales <90020224+cesargatica@users.noreply.github.com>
2022-11-27 19:31:50 -0600

    Lang Creator App
    Version 1

---
+++ pages/index.js
@@ -0,0 +1,100 @@
+import { useState } from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+import buildspaceLogo from '../assets/buildspace-logo.png';
+
+const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('')
+const [isGenerating, setIsGenerating] = useState(false)
+
+const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
+  
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+  console.log("OpenAI replied...", output.text)
+
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
+  const onUserChangedText = (event) => {
+    setUserInput(event.target.value);
+  };
+  return (
+    <div className="root">
+      <div className="container">
+        <div className="header">
+          <div className="header-title1">
+            <h1>LangCreator App</h1>
+          </div>
+          <div className="header-title">
+            <h2>Awesome Text Generator</h2>
+          </div>
+          <div className="header-subtitle">
+            <h2>You write a nice prompt here and will obtain AI generated Text</h2>
+          </div>
+        </div>
+        {/* Add this code here*/}
+        <div className="prompt-container">
+  <textarea
+    placeholder="start typing here"
+    className="prompt-box"
+    value={userInput}
+    onChange={onUserChangedText}
+  />
+  {/* New code I added here */}
+  <div className="prompt-buttons">
+  <a
+    className={isGenerating ? 'generate-button loading' : 'generate-button'}
+    onClick={callGenerateEndpoint}
+  >
+    <div className="generate">
+    {isGenerating ? <span class="loader"></span> : <p>Generate</p>}
+    </div>
+  </a>
+</div>
+   {/* New code I added here */}
+   {apiOutput && (
+  <div className="output">
+    <div className="output-header-container">
+      <div className="output-header">
+        <h3>Output</h3>
+      </div>
+    </div>
+    <div className="output-content">
+      <p>{apiOutput}</p>
+    </div>
+  </div>
+)}
+  </div>
+  </div>
+      <div className="badge-container grow">
+        <a
+          href="https://buildspace.so/builds/ai-writer"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="badge">
+            <Image src={buildspaceLogo} alt="buildspace logo" />
+            <p>build with buildspace</p>
+          </div>
+          <div className="badge">
+            <p>Powered by Cesar Gatica, 2022</p>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
